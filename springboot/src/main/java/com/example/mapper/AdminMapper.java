20439d40981c1d8b3f4e8d3137b6a3258d17e984
Apple <Kreveyn@gmail.com>
2023-12-22 13:29:26 +0800

    student

--- springboot/src/main/java/com/example/mapper/AdminMapper.java
+++
@@ -1,9 +0,0 @@
-package com.example.mapper;
-
-import com.example.entity.Admin;
-import org.apache.ibatis.annotations.Select;
-
-public interface AdminMapper {
-    @Select("select * from admin where username = #{username}")
-    Admin selectByUsername(String username);
-}
